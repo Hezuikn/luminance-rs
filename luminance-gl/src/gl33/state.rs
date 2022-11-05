3cb50174384eb33ac003dc41f851d80981cad80e
Hezuikn <68823892+Hezuikn@users.noreply.github.com>
2022-11-05 22:42:55 +0000

    Update state.rs

--- luminance-gl/src/gl33/state.rs
+++ luminance-gl/src/gl33/state.rs
@@ -482,7 +482,7 @@ impl GLState {
     &mut self.binding_stack
   }
 
-  pub(crate) fn create_texture(&mut self) -> GLuint {
+  pub fn create_texture(&mut self) -> GLuint {
     self.texture_swimming_pool.pop().unwrap_or_else(|| {
       let mut texture = 0;
 
